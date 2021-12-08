4810122df2fb308afc8486cf0df57d565bc39b0e
Fabio <fabiof@dudamobile.com>
2021-12-08 15:09:39 +0200

    shirel says to do that

--- src/main/kotlin/com/duda/battlesnake/BattlesnakeApplication.kt
+++ src/main/kotlin/com/duda/battlesnake/BattlesnakeApplication.kt
@@ -2,10 +2,37 @@ package com.duda.battlesnake
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class BattlesnakeApplication
 
 fun main(args: Array<String>) {
     runApplication<BattlesnakeApplication>(*args)
+}
+
+@RestController
+class Controller {
+
+    @GetMapping
+    fun get(): GetDataModel {
+        return GetDataModel()
+    }
+
+    @PostMapping("start")
+    fun start(): String {
+        return ""
+    }
+
+    @PostMapping("move")
+    fun move(): MoveDataModel {
+        return MoveDataModel()
+    }
+
+    @PostMapping("end")
+    fun end(): String {
+        return "end"
+    }
 }
