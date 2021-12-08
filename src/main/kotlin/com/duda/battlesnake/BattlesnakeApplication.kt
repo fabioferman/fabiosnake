6b17c60f62b8089256ed84246a4ea3c3d8975975
Fabio <fabiof@dudamobile.com>
2021-12-08 15:13:10 +0200

    shirel says to do that

--- src/main/kotlin/com/duda/battlesnake/BattlesnakeApplication.kt
+++ src/main/kotlin/com/duda/battlesnake/BattlesnakeApplication.kt
@@ -36,3 +36,17 @@ class Controller {
         return "end"
     }
 }
+
+class GetDataModel {
+    var apiversion = "1";
+    var author = "fabio";
+    var color = "#888888";
+    var head = "default";
+    var tail = "default";
+    var version = "0.0.1-beta";
+}
+
+class MoveDataModel {
+    var move = "up";
+    var shout = "Moving up!";
+}
